2d9f61b07b5d3f0665ea4a0ff870e03cd2901b77
AvroraPolnareff <araintheskywith@gmail.com>
2020-08-17 18:32:47 +0300

    App has a Divider now!!!

---
+++ src/components/Divider.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Divider = styled.div`
+    margin: 0.2rem 0;
+    background-color: #ebebeb;
+    width: 100%;
+    height: 2px;
+    border-radius: 2px;
+`
