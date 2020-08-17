86ad5c9785ade79380e450501d63063db56b8517
AvroraPolnareff <araintheskywith@gmail.com>
2020-08-17 18:04:46 +0300

    App with styles now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from "styled-components";
 
 
 function App() {
   return (
-    <div className="App">
+    <StyledApp>
     
-    </div>
+    </StyledApp>
   );
 }
+
+export const StyledApp = styled.div`
+    font-size: 12px;
+    font-weight: 700;
+    margin: 0;
+    font-family: "Gilroy", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+`
 
 export default App;
