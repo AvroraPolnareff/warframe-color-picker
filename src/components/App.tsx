a0d45053438c5d5de2b96f563c67a6c157070370
AvroraPolnareff <araintheskywith@gmail.com>
2020-08-18 12:07:13 +0300

    App has a color picker now ^^

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import TargetScheme from "./TargetScheme";
+import {ColorPicker} from "./ColorPicker";
+import Color from "color";
 
 
 function App() {
-  
   const initColors = ["#f8f5ed", "#525757", '#a64731', "#c0cbcf", "#dffefb", "#53bcb1", "#dffefb", "#53bcb1"]
+  const [colors, setColors] = useState(initColors)
+  const [currentColor, setCurrentColor] = useState(0)
+  
+  const onColorChange = (color: Color) => {
+    const newColors = colors.slice()
+    newColors[currentColor] = color.hex()
+    setColors(newColors)
+  }
+  
   
   return (
-    <StyledApp>
-      <TargetScheme colors={initColors} onCellChange={console.log}/>
+    <StyledApp style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+      <ColorPicker color={Color().hex(colors[currentColor])} onColorChange={onColorChange}/>
+      <TargetScheme colors={colors} onCellChange={setCurrentColor}/>
     </StyledApp>
   );
 }
 
 export const StyledApp = styled.div`
+    height: 100vh;
     font-size: 12px;
     font-weight: 700;
     margin: 0;
---
+++ src/components/ColorPicker.tsx
@@ -0,0 +1,207 @@
+import React, {FC, useEffect, useState} from "react";
+import Color from "color";
+import {Window} from "./shared/Window";
+import {FlexColumnCenter} from "./shared/FlexColumnCenter";
+import styled from "styled-components";
+import {Divider} from "./shared/Divider";
+import header from "../assets/color_picker_header.svg"
+import picker from "../assets/picker.png"
+
+interface ColorPickerProps {
+  color: Color,
+  onColorChange: (color: Color) => void
+}
+
+export const ColorPicker: FC<ColorPickerProps> = ({onColorChange, color}) => {
+  
+  return (
+    <Window width={12.3}>
+      <FlexColumnCenter>
+        <FlexRow>
+          <HeaderWrapper>
+            <img src={header}/>
+          </HeaderWrapper>
+          <HexInput
+            color={color}
+            onChange={(e) => onColorChange(Color().hex(e.target.value))}
+          />
+        </FlexRow>
+        <PickerImg src={picker}/>
+        <Divider/>
+      </FlexColumnCenter>
+      <NumbersPicker color={color} onColorChange={onColorChange}/>
+    </Window>
+  )
+}
+
+interface NumbersPickerProps {
+  color: Color,
+  onColorChange: (color: Color) => void
+}
+
+const NumbersPicker : FC<NumbersPickerProps> = ({color, onColorChange}) => {
+  
+  const onChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    switch (e.target.name) {
+      case "red":
+        onColorChange(color.red(parseInt(e.target.value)))
+        break
+      case "green":
+        onColorChange(color.green(parseInt(e.target.value)))
+        break
+      case "blue":
+        onColorChange(color.blue(parseInt(e.target.value)))
+        break
+      case "hue":
+        onColorChange(color.hue(parseInt(e.target.value)))
+        break
+      case "saturation":
+        onColorChange(color.saturate(parseInt(e.target.value)))
+        break
+      case "lightness":
+        onColorChange(color.lightness(parseInt(e.target.value)))
+        break
+      default:
+        return
+    }
+  }
+  
+  return (
+    <StyledPicker>
+      <Grid2X4>
+        <ColorSchemeName>RGB</ColorSchemeName>
+        <ColorInput min={0} max={255} name={"red"} onChange={onChange} color="#dba3a3" value={color.red()}/>
+        <ColorInput min={0} max={255} name={"green"} onChange={onChange} color="#a3dba3" value={color.green()}/>
+        <ColorInput min={0} max={255} name={"blue"} onChange={onChange} color="#a3a3db" value={color.blue()}/>
+        <ColorSchemeName>HSL</ColorSchemeName>
+        <ColorInput name={"hue"} onChange={onChange} value={Math.round(color.hue())}/>
+        <ColorInput name={"saturation"} onChange={onChange} value={Math.round(color.saturationl())}/>
+        <ColorInput name={"lightness"} onChange={onChange} value={Math.round(color.lightness())}/>
+      </Grid2X4>
+    </StyledPicker>
+  )
+}
+
+const StyledPicker = styled.div`
+    margin-top: 0.3em;
+    font-size: 14px
+`
+
+const ColorInput = styled.input`
+    display: flex;
+    align-items: center;
+    background-color: ${props => props.color || props.theme.colors.badge};
+    color: ${props => props.theme.colors.badgeText};
+    padding: 0.1rem 0.3rem;
+    max-height: 1.2rem;
+    margin: 0 0.2rem;
+    border-radius: 0.4rem;
+    font-weight: 600;
+    font-size: 14px;
+    width: 1.5rem;
+    border: none;
+    
+    &:focus {
+        outline: none;
+    }
+`
+
+
+const Grid2X4 = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    row-gap: 0.3rem;
+    justify-items: start;
+    justify-content: start;
+    //margin-right: 2.6rem;
+    width: fit-content;
+    margin-left: 0.5rem;
+`
+
+const ColorSchemeName = styled.div`
+    padding-right: 0.3em
+`
+
+interface HexInputProps {
+  color: Color;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const HexInput : FC<HexInputProps> = ({onChange, color }) => {
+  const [validHex, setValidHex] = useState(true)
+  const [inputField, setInputField] = useState("#909090")
+  const [userTyping, setUserTyping] = useState(false)
+  const [timer, setTimer ] = useState(0)
+  const [onChangeTimeout, setOnChangeTimeout] = useState(0)
+  
+  useEffect(() => {
+    if (!userTyping) {
+      setInputField(color.hex())
+      setValidHex(true)
+    }
+  },[color])
+  
+  const changeHex = (e : React.ChangeEvent<HTMLInputElement>) => {
+    clearTimeout(timer)
+    clearTimeout(onChangeTimeout)
+    setTimer(setTimeout(() =>setUserTyping(false), 1000))
+    try {
+      if (e.target.value.length <= 7) {
+        Color().hex(e.target.value)
+        setValidHex(true)
+        e.persist()
+        setInputField(e.target.value)
+        setOnChangeTimeout(setTimeout(() => onChange(e), 2000))
+      }
+      
+    } catch (error) {
+      setValidHex(false)
+      setInputField(e.target.value)
+    }
+  }
+  
+  return (
+    <StyledHexInput
+       type={"text"}
+       value={inputField}
+      onChange={changeHex} valid={validHex}
+    />
+  )
+}
+
+const StyledHexInput = styled.input<{valid: boolean}>`
+    display: flex;
+    align-items: center;
+    background-color: ${props => props.valid ? props.theme.colors.badge : "#dba3a3"};
+    color: ${props => props.theme.colors.badgeText};
+    padding: 0.1rem 0.3rem;
+    max-height: 1.2rem;
+    margin: 0 0.2rem;
+    border-radius: 0.4rem;
+    font-weight: 600;
+    font-size: 14px;
+    width: 3.7rem;
+    border: none;
+    
+    &:focus {
+        outline: none;
+    }
+`
+
+const HeaderWrapper = styled.div`
+    position: relative;
+    top: -2em;
+    left: -3em;
+
+
+`
+
+const FlexRow = styled.div`
+    display: flex;
+    max-height: 2.3rem;
+`
+
+const PickerImg = styled.img`
+    margin-bottom: 0.5em;
+`
