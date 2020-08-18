9940e46f4baf62241446d0d8092842cb23483db1
AvroraPolnareff <araintheskywith@gmail.com>
2020-08-18 14:11:07 +0300

    Updates

---
+++ precache-manifest.88432e1488573793fa54b566a11bf5da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c88ef5b1bb796b9d419bb618c86c844",
+    "url": "/warframe-color-picker-ts/index.html"
+  },
+  {
+    "revision": "75085ce6930e32e83001",
+    "url": "/warframe-color-picker-ts/static/js/2.bf869c8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/warframe-color-picker-ts/static/js/2.bf869c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682fc68624dc784ce5b3",
+    "url": "/warframe-color-picker-ts/static/js/main.127ffc63.chunk.js"
+  },
+  {
+    "revision": "580e6a4703e83d21f834",
+    "url": "/warframe-color-picker-ts/static/js/runtime-main.8d4a33d4.js"
+  },
+  {
+    "revision": "e496a7b3619e83d051344be2f97c941f",
+    "url": "/warframe-color-picker-ts/static/media/color_picker_header.e496a7b3.svg"
+  },
+  {
+    "revision": "4903b5165290371459883457bbdeb0dc",
+    "url": "/warframe-color-picker-ts/static/media/picker.4903b516.png"
+  },
+  {
+    "revision": "4b80c86bca77373b178925fd48deb57d",
+    "url": "/warframe-color-picker-ts/static/media/suggestions.4b80c86b.svg"
+  }
+]);
