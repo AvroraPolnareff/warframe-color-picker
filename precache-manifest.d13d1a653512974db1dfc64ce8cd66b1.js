b37bf285edff2ec1549203e77ffbb39fde5668bc
AvroraPolnareff <araintheskywith@gmail.com>
2020-08-24 07:38:52 +0300

    Updates

---
+++ precache-manifest.d13d1a653512974db1dfc64ce8cd66b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a12f8b5f96c65545d6830f0fcb8823",
+    "url": "/warframe-color-picker-ts/index.html"
+  },
+  {
+    "revision": "be50740de057e1581040",
+    "url": "/warframe-color-picker-ts/static/js/2.e8c543a7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/warframe-color-picker-ts/static/js/2.e8c543a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578b80c78725a1b7c604",
+    "url": "/warframe-color-picker-ts/static/js/main.45234731.chunk.js"
+  },
+  {
+    "revision": "580e6a4703e83d21f834",
+    "url": "/warframe-color-picker-ts/static/js/runtime-main.8d4a33d4.js"
+  },
+  {
+    "revision": "665afc47f57aada5850f9fc3b802de84",
+    "url": "/warframe-color-picker-ts/static/media/SUGGESTIONS.665afc47.svg"
+  },
+  {
+    "revision": "a543d2ffde145cefa948e17f4855d48a",
+    "url": "/warframe-color-picker-ts/static/media/exitButton.a543d2ff.svg"
+  },
+  {
+    "revision": "f66748bc2666f1bf088774fd7ec42282",
+    "url": "/warframe-color-picker-ts/static/media/layoutIcon.f66748bc.svg"
+  },
+  {
+    "revision": "02c29c601dcc6c5da2e0aabefe8f027b",
+    "url": "/warframe-color-picker-ts/static/media/tipOfADay.02c29c60.svg"
+  },
+  {
+    "revision": "63f5f9abb4339f8b9b88cd7104af588b",
+    "url": "/warframe-color-picker-ts/static/media/waves.63f5f9ab.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/warframe-color-picker-ts/precache-manifest.01bc5328f60ab53e8b771bbe64d8d284.js"
+  "/warframe-color-picker-ts/precache-manifest.d13d1a653512974db1dfc64ce8cd66b1.js"
 );
 
 self.addEventListener('message', (event) => {
