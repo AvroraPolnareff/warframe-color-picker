7776fbfccd77f74bd59b7bb6b7ad0b802a1949e7
AvroraPolnareff <araintheskywith@gmail.com>
2020-09-04 07:43:25 +0300

    🎨 refactor: PaletteCell has selected prop now

--- src/components/SelectedColor.tsx
+++ src/components/SelectedColor.tsx
@@ -1,7 +1,7 @@
-import React, {FC, useEffect, useRef} from "react"
+import React, {FC} from "react"
 import {Window} from "./shared/Window";
 import {FlexColumnCenter} from "./shared/FlexColumnCenter";
-import styled, {keyframes} from "styled-components";
+import styled, {css, keyframes} from "styled-components";
 import {Position} from "../common/Palette";
 import {palettes} from "../common/palettes";
 import waves from "../assets/waves.svg"
@@ -11,22 +11,33 @@ interface SelectedColorProps {
   colorPosition: Position
 }
 
-export const SelectedColor : FC<SelectedColorProps> = ({paletteName, colorPosition}) => {
+export const SelectedColor: FC<SelectedColorProps> = ({paletteName, colorPosition}) => {
   return (
     <Window width={11}>
       <FlexColumnCenter>
-        <img src={waves} style={{width: "8.7em", marginTop: "0.2em", marginBottom: "0.1em", pointerEvents: "none", userSelect: "none"}}/>
+        <img src={waves} style={{
+          width: "8.7em",
+          marginTop: "0.2em",
+          marginBottom: "0.1em",
+          pointerEvents: "none",
+          userSelect: "none"
+        }}/>
         <Header>SELECTED COLOR</Header>
-        <img src={waves} style={{width: "8.7em", marginTop: "0", marginBottom: "0.5em", pointerEvents: "none", userSelect: "none"}}/>
+        <img src={waves} style={{
+          width: "8.7em",
+          marginTop: "0",
+          marginBottom: "0.5em",
+          pointerEvents: "none",
+          userSelect: "none"
+        }}/>
         <PaletteName>{paletteName}</PaletteName>
-
-          <WarframePalette size={1.75} paletteName={paletteName} colorPosition={colorPosition}/>
-
+        
+        <WarframePalette size={1.75} paletteName={paletteName} colorPosition={colorPosition}/>
+      
       </FlexColumnCenter>
     </Window>
   )
 }
-
 
 const Header = styled.div`
     font-weight: 300;
@@ -61,40 +72,57 @@ interface WarframePaletteProps {
   colorPosition?: Position
 }
 
-export const WarframePalette : FC<WarframePaletteProps> = ({ size, paletteName, colorPosition }) => {
+export const WarframePalette: FC<WarframePaletteProps> = ({size, paletteName, colorPosition}) => {
   const palette = palettes.filter(palette => palette.name === paletteName)[0]
   
   return (
     <StyledWarframePalette size={size}>
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(5, 1fr)",
-        gridTemplateRows: "repeat(18, 1fr)",
-        justifyItems: "center",
-        alignItems: "center"
-      }}>
       {palette.colors.map(color => (
-        !(color.position.x === colorPosition?.x && colorPosition.y === color.position.y) ?
-          <PaletteCell size={size} color={color.hex}/> :
-          <BackgroundGradient size={size}><SelectedCell size={size} color={color.hex}/></BackgroundGradient>
+        <PaletteCell
+          selected={color.position.x === colorPosition?.x && colorPosition?.y === color.position.y}
+          color={color.hex}
+          size={size}
+        />
       ))}
-      </div>
     </StyledWarframePalette>
   )
 }
 
-const StyledWarframePalette = styled.div<{size: number}>`
-  //display: grid;
-  //grid-template-columns: repeat(5, 1fr);
+const StyledWarframePalette = styled.div<{ size: number }>`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(18, 1fr);
+  justify-items: center;
+  align-items: center;
   border: 0.2em solid ${props => props.theme.colors.tertiary};
   border-radius: 0.633em;
   overflow: hidden;
 `
 
-const PaletteCell = styled.div.attrs(props => ({style: {backgroundColor: props.color}}))<{size: number, color: string}>`
-  height: ${props => props.size }em;
-  width: ${props => props.size }em;
+const PaletteCell: FC<{ size: number, color: string, selected: boolean }> = ({size, color, selected}) => {
+  return (
+    <>
+      {selected
+          ? <BackgroundGradient size={size}>
+              <StyledPaletteCell size={size} color={color} selected={selected}/>
+            </BackgroundGradient>
+          
+          : <StyledPaletteCell size={size} color={color} selected={selected}/>
+      }
+    
+    </>
+  )
+}
+
+const StyledPaletteCell = styled.div.attrs(props => ({style: {backgroundColor: props.color}}))
+  < {size: number, color: string, selected: boolean} > `
+  height: ${props => props.size}em;
+  width: ${props => props.size}em;
   transition: background-color 0.3s linear;
+  ${props => props.selected && css`
+    max-height: ${props.size - 0.4}em;
+    max-width: ${props.size - 0.4}em;
+  `}
 `
 
 const moveGradient = keyframes`
@@ -103,9 +131,9 @@ const moveGradient = keyframes`
   }
 `
 
-const BackgroundGradient = styled.div<{size: number}>`
-  height: ${props => props.size }em;
-  width: ${props => props.size }em;
+const BackgroundGradient = styled.div<{ size: number }>`
+  height: ${props => props.size}em;
+  width: ${props => props.size}em;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -120,9 +148,5 @@ const BackgroundGradient = styled.div<{size: number}>`
   animation: ${moveGradient} 4s alternate infinite;
 `
 
-const SelectedCell = styled(PaletteCell)<{size: number, color: string}>`
-    max-height: ${props => props.size - 0.4}em;
-    max-width: ${props => props.size - 0.4}em;
 
-`
 
