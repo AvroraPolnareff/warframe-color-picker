a5bf8d168ba6c2b9740b6bb4c12947358cd40066
AvroraPolnareff <araintheskywith@gmail.com>
2020-11-22 22:02:05 +0300

    change colors of import placeholder text

--- src/components/ImportModal.tsx
+++ src/components/ImportModal.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useRef, useState} from "react";
+import React, {FC, HTMLProps, useRef, useState} from "react";
 import styled from "styled-components";
 import {Modal} from "./shared/Modal";
 import {Button} from "./shared/Button";
@@ -53,19 +53,39 @@ export const ImportModal: FC<ImportModalProps> = ({show, onAccept, onExit, onScr
   }
   
   
+
   return (
-    <Modal show={show} width={27} name={"Scheme Import"} description={"Insert the code below"} onExit={onExit}>
-      <TextArea onChange={e => setContent(e.target.value)} value={content} onPaste={onPaste}/>
+    <Modal
+      show={show}
+      width={27}
+      name={"Scheme Import"}
+      description={"Insert the code below"}
+      onExit={onExit}
+    >
+      {/*@ts-ignore*/}
+      <TextArea onChange={e => setContent(e.target.value)} value={content} onPaste={onPaste}>
+        Paste your <span style={{color: "#A3A3DB"}}>code</span> or <span style={{color: "#A3A3DB"}}>scheme screenshot</span> here...
+      </TextArea>
       <div style={{display: "flex", justifyContent: "space-between"}}>
         <div style={{textAlign: "left"}}>
-          <input ref={inputRef} type="file" style={{position: "fixed", top: -100}} onChange={onScreenshotImportChange}
-                 name="screenshot" id="screenshot"/>
-  
+          <input
+            ref={inputRef}
+            type="file"
+            style={{position: "fixed", top: -100}}
+            onChange={onScreenshotImportChange}
+            name="screenshot" id="screenshot"
+          />
           <Button as="label" htmlFor="screenshot"  round small>From Screenshot</Button>
         </div>
         <div style={{textAlign: "right"}}>
           
-          <Button round small warning onClick={() => setContent("")} style={{marginRight: '0.4em'}}>clear</Button>
+          <Button
+            round small warning
+            onClick={() => setContent("")}
+            style={{marginRight: '0.4em'}}
+          >
+            clear
+          </Button>
           <Button round small primary onClick={() => onAccept(content)}>accept</Button>
         </div>
       </div>
@@ -73,8 +93,31 @@ export const ImportModal: FC<ImportModalProps> = ({show, onAccept, onExit, onScr
   )
 }
 
-const TextArea = styled.textarea.attrs(() => ({
-  rows: 3, autofocus: "true", placeholder: "Paste your code or scheme screenshot here...", spellCheck: "false"
+const TextArea: FC<HTMLProps<HTMLTextAreaElement>> = ({children, ...props}) => {
+  const [placeholder, setPlaceholder] = useState(true)
+  
+  return (
+    <Relative>
+      <StyledTextArea
+        onChange={props.onChange}
+        value={props.value}
+        onPaste={props.onPaste}
+        onFocus={() => setPlaceholder(false)}
+        onBlur={() => setPlaceholder(true)}
+      />
+        <Placeholder style={{display: placeholder? "block" : "none"}}>{children}</Placeholder>
+    </Relative>
+  )
+}
+
+const Relative = styled.div`
+  position: relative;
+  color: ${props => props.theme.colors.darken.secondary};
+`
+
+const StyledTextArea = styled.textarea.attrs(() => ({
+  rows: 3, autofocus: "true",
+  spellCheck: "false"
 }))`
   border: none;
   margin: 0;
@@ -83,10 +126,15 @@ const TextArea = styled.textarea.attrs(() => ({
   resize: none;
   outline: none;
   color: ${props => props.theme.colors.darken.secondary};
-  overflow: hidden;
+  overflow-y: auto;
   
   &:focus {
     border: none;
   }
 `
 
+const Placeholder = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`
