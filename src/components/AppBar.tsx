258d906fccc2e409b19b93fe526b425a23972dce
AvroraPolnareff <AraInTheSkyWith@gmail.com>
2021-02-20 13:07:48 +0300

    moving app bar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -17,17 +17,24 @@ export const AppBar : FC = () => {
 const StyledAppBar = styled.div`
   display: flex;
   justify-content: center;
-  
+  height: 2em;
 `
 
 const CenterContainer = styled.div`
   display: flex;
   width: max-content;
+  height: max-content;
+  color: transparent;
+  transform: translate(0, -70%);
+  transition: all 0.3s ease;
+  :hover {
+    color: ${({theme}) => theme.colors.windowBackground};
+    transform: translate(0, 0);
+  }
 `
 
 const Entry = styled.div`
   padding: 0.05em 0.3em;
-  color: ${({theme}) => theme.colors.windowBackground};
   background-color: ${({theme}) => theme.colors.secondary};
   border-bottom-left-radius: 0.7em;
   border-bottom-right-radius: 0.7em;
