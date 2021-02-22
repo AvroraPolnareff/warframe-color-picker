462fd76bfd2d72afeece86e55727bc7f544d1316
AvroraPolnareff <AraInTheSkyWith@gmail.com>
2021-02-22 05:12:20 +0300

    use main logic hook in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -23,107 +23,36 @@ import targetSchemeToSuggestions from "../assets/Wires (Col Pic -_ Tar Sch).svg"
 import targetSchemeToColorPicker from "../assets/Wires (Tar Sch -_ Col Pic).svg"
 import suggestionsToSelectedColor from "../assets/Wires (Sugg -_ Sel Col).svg"
 import { AppBar } from './AppBar';
-
-export const initManualColors = Array(48).fill("")
-
-export const initAvailablePalettes = palettes.map((palette) => palette.name)
+import {useColorPickerLogic} from "../hooks/color-picker-logic";
 
 function App() {
 
-  const initMatchedColor = {
-    distance: 0,
-    color: "#000000",
-    paletteName: "Classic",
-    position: {x: 0, y: 0},
-    uid: "3274823"
-  }
+  const {
+    availablePalettes,
+    showPalettesModal,
+    setShowPalettesModal,
+    onPaletteClick,
+    clearAvailablePalettes,
+    showAllAvailablePalettes,
 
-  const [paletteColors, setPaletteColors] = useStickyState(initManualColors, "manualColors")
-  const [currentColorIndex, setCurrentColorIndex] = useState(0)
-  const [matchedColors, setMatchedColors] = useState<MatchedColor[]>([])
-  const [isColorChanging, setIsColorChanging] = useState(false)
-  const [selectedColor, setSelectedColor] = useState<MatchedColor>(initMatchedColor)
+    showImportModal,
+    onAcceptImport,
+    setShowImportModal,
+    onScreenshotImport,
 
-  const [showImportModal, setShowImportModal] = useState(false)
-  const [showPalettesModal, setShowPalettesModal] = useState(false)
-  const [availablePalettes, setAvailablePalettes] = useStickyState<string[]>(initAvailablePalettes, "availablePalettes")
+    paletteColors,
+    onCellClick,
 
-  const debounced = useRef(debounce((fn: () => void) => fn(), 150, {trailing: true, leading: false}))
+    getCurrentColor,
+    onColorChange,
 
+    matchedColors,
+    onSuggestionClick,
+    isColorChanging,
+    onOverrideColor,
 
-  const getCurrentColor = (): string => {
-    const color = paletteColors[currentColorIndex]
-    if (color === "") return "#000000"
-    return color
-  }
-
-  const updateSuggestions = () => {
-    const filteredPalettes = palettes.filter(palette => availablePalettes.indexOf(palette.name) !== -1)
-    const closestColors = findClosestColors(getCurrentColor(), filteredPalettes, 50)
-    setMatchedColors(closestColors)
-    setSelectedColor(closestColors[0])
-    setIsColorChanging(false)
-  }
-
-  useEffect(() => {
-    updateSuggestions()
-  }, [])
-
-  useEffect(() => {
-    if (!isColorChanging) {
-      setIsColorChanging(true)
-    }
-    debounced.current(updateSuggestions)
-  }, [paletteColors, currentColorIndex, availablePalettes])
-const onCellClick = (index: number, e: React.MouseEvent) => {
-    if (e.type === "contextmenu") {
-      e.preventDefault();
-      const newPaletteColors = [...paletteColors];
-      newPaletteColors[index] = "";
-      setPaletteColors(newPaletteColors);
-    }
-    setCurrentColorIndex(index);
-  }
-
-  const onColorChange = (color: Color) => {
-    const newColors = [...paletteColors]
-    newColors[currentColorIndex] = color.toString()
-    setPaletteColors(newColors)
-  }
-
-  const onSuggestionClick = (key: string) => {
-    const filteredColor = matchedColors.filter(({uid}) => uid === key)[0]
-    if (filteredColor.uid !== selectedColor.uid) {
-      setSelectedColor(filteredColor);
-    }
-  }
-
-  const onAcceptImport = (data: string) => {
-    try {
-      const importData = convertExportStringToColors(data)
-      setPaletteColors(importData)
-      setShowImportModal(false)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  const onPaletteClick = (paletteName: string) => {
-    const isExists = availablePalettes.indexOf(paletteName) !== -1
-    if (isExists) {
-      if (availablePalettes.length === 1) {
-        setAvailablePalettes(["Classic"])
-        return
-      }
-      setAvailablePalettes(availablePalettes.filter((el: string) => el !== paletteName))
-    } else {
-      setAvailablePalettes([...availablePalettes, paletteName])
-    }
-  }
-
-  const onOverrideColor = () => {
-    onColorChange(Color(selectedColor.color))
-  }
+    selectedColor,
+  } = useColorPickerLogic();
 
   return (
     <StyledApp>
@@ -135,8 +64,8 @@ const onCellClick = (index: number, e: React.MouseEvent) => {
           availablePalettes={availablePalettes}
           show={showPalettesModal}
           onPaletteClick={onPaletteClick}
-          onDisableAll={() => setAvailablePalettes(["Classic"])}
-          onEnableAll={() => setAvailablePalettes(initAvailablePalettes)}
+          onDisableAll={clearAvailablePalettes}
+          onEnableAll={showAllAvailablePalettes}
           onExit={() => setShowPalettesModal(false)}
         />
         : null
@@ -148,14 +77,7 @@ const onCellClick = (index: number, e: React.MouseEvent) => {
         onExit={() => {
           setShowImportModal(false)
         }}
-        onScreenshotImport={(colors => {
-          setPaletteColors([
-            ...paletteColors.slice(0, Math.floor(currentColorIndex / 8) * 8),
-            ...colors,
-            ...paletteColors.slice(Math.floor(currentColorIndex / 8 + 1) * 8)
-          ]);
-          setShowImportModal(false)
-        })}
+        onScreenshotImport={onScreenshotImport}
       />
 
       <AppBar/>
