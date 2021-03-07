7eb86d41f9827478762054705eb83e06383b3ae3
AvroraPolnareff <AraInTheSkyWith@gmail.com>
2021-03-07 17:33:19 +0300

    add screen transitions

--- src/components/ScreensSwitcher.tsx
+++ src/components/ScreensSwitcher.tsx
@@ -1,4 +1,4 @@
-import {FC, useContext} from "react";
+import React, {FC, useContext} from "react";
 import {CurrentScreenContext} from "../providers/CurrentScreenProvider";
 import {Layout, SettingsContext} from "../providers/SettingsProvider";
 import {Classic} from "./layouts/Classic";
@@ -6,17 +6,43 @@ import {Expanded} from "./layouts/Expanded";
 import {Layouts} from "./screens/Layouts";
 import {Screen} from "../providers/CurrentScreenProvider";
 import {Languages} from "./screens/Languages";
-
+import {SwitchTransition, Transition} from "react-transition-group";
+import styled from "styled-components/macro";
+import {TransitionProps} from "react-transition-group/Transition";
 
 export const ScreensSwitcher = () => {
   const {screen} = useContext(CurrentScreenContext);
   const {layout} = useContext(SettingsContext);
-  switch (screen) {
-    case Screen.COLOR_PICKER: return <CurrentLayout layout={layout}/>
-    case Screen.LAYOUT_SELECTION: return <Layouts/>
-    case Screen.LANGUAGE_SELECTION: return <Languages/>
-  }
+  return (
+    <SwitchTransition mode={"out-in"}>
+      <FadeTransition
+        key={screen}
+        timeout={250}
+        unmountOnExit
+        mountOnEnter
+      >
+        {
+          screen === Screen.COLOR_PICKER ? <CurrentLayout layout={layout}/> :
+          screen === Screen.LAYOUT_SELECTION ? <Layouts/> :
+          screen === Screen.LANGUAGE_SELECTION && <Languages/>
+        }
+      </FadeTransition>
+    </SwitchTransition>
+  )
 }
+
+const FadeDiv = styled.div<{state: string}>`
+  transition: 0.3s ease;
+  opacity: ${({ state }) => (state === "entered" ? 1 : 0)};
+  display: ${({ state }) => (state === "exited" ? "none" : "block")};
+`;
+
+const FadeTransition: FC<TransitionProps> = ({ children, ...rest }) => (
+  <Transition {...rest}>
+    {state => <FadeDiv state={state}>{children}</FadeDiv>}
+  </Transition>
+);
+
 
 const CurrentLayout: FC<{layout: Layout}> = ({layout}) => {
   switch (layout) {
