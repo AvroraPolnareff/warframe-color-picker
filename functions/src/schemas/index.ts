985ec297df5ddcad5283cd536b02a922d3f4460c
AvroraPolnareff <AraInTheSkyWith@gmail.com>
2021-04-10 20:34:51 +0300

    schema error fix

--- functions/src/schemas/index.ts
+++ functions/src/schemas/index.ts
@@ -2,5 +2,5 @@ import * as Joi from "joi";
 
 export const paletteSchema = Joi.object({
   name: Joi.string().max(55),
-  colors: Joi.array().max(49).items(Joi.string().max(7))
+  colors: Joi.array().max(49).items(Joi.string().allow("").max(8))
 })
