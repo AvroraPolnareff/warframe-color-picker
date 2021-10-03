06f9dc44126758028ed50b841de665a9c7f38e55
AvroraPolnareff <Araintheskywith@gmail.com>
2021-10-03 08:59:03 +0300

    add generated icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,17 @@ const Index = () => {
         title="Warframe Color Picker"
         description="I'm your very own Cephalon Cosmetologist from now on, Operator! 😎 Find closest colors | Import colors schemes from screenshots | Share with your friends"
         canonical="https://www.warframecolorpicker.app/"
-        // additionalMetaTags={[
-        //   {name: "theme-color", content: "#ffffff"}
-        // ]}
         additionalLinkTags={[
-          {rel: "manifest", href: "/manifest.json"},
+          {rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
+          {rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5"},
+          {rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
+          {rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
+          {rel: "manifest", href: "/site.webmanifest"},
           {rel:"shortcut icon", href: "/favicon.ico", type:"image/x-icon"}
         ]}
         additionalMetaTags={[
+          {name: "msapplication-TileColor", content: "#5bbad5"},
+          {name: "theme-color", content: "#000000"},
           {name: "yandex-verification", content: "b316278f5276b429"}
         ]}
       />
