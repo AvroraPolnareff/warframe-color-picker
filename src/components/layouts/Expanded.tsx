eff049c09f56bc1eb50a010cb679bd6931762867
Asta Ness <74872505+Morisabeau@users.noreply.github.com>
2021-11-30 00:01:03 +0300

    Update Expanded.tsx

--- src/components/layouts/Expanded.tsx
+++ src/components/layouts/Expanded.tsx
@@ -89,7 +89,7 @@ export const Expanded = () => {
               />
             </div>
             <Link
-              href="https://github.com/AvroraPolnareff/warframe-color-picker/blob/master/README.md#how-to-use"
+              href="https://github.com/AvroraPolnareff/warframe-color-picker/blob/master/docs/GUIDE.md"
               height={2.3}
               width={11}>{t("colorPicker.howToUse")}</Link>
             <div style={{display: "flex"}}>
