e138a4c8d355e77e852c95ef0e04d7129647e6fe
Asta Ness <74872505+Morisabeau@users.noreply.github.com>
2021-11-30 00:07:57 +0300

    Update Classic.tsx
    yes

--- src/components/layouts/Classic.tsx
+++ src/components/layouts/Classic.tsx
@@ -88,7 +88,7 @@ export const Classic = () => {
               </div>
             </Wires>
             <Link
-              href="https://github.com/AvroraPolnareff/warframe-color-picker/blob/master/docs/GUIDE.md"
+              href="https://github.com/AvroraPolnareff/warframe-color-picker/blob/master/docs/GUIDE.md#how-to-use"
               height={2.3}
               width={11}>{t("colorPicker.howToUse")}</Link>
             <div style={{display: "flex"}}>
