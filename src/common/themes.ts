84a65c95364322b8f85f87f97e5f11fc03abd385
Aurora <Polnareff>
2022-05-26 18:29:32 +0300

    change colors

--- src/common/themes.ts
+++ src/common/themes.ts
@@ -3,9 +3,9 @@ import Color from "color";
 
 export const colors: Colors = {
   primary: "#A5E8E8",
-  secondary: "#DADADA",
-  tertiary: "#ebebeb",
-  quaternary: "#ebebeb",
+  secondary: "#bebebe",
+  tertiary: "#d7d7d7",
+  quaternary: "#d7d7d7",
   danger: "#dba3a3",
   success: "#A3DBA3",
   link: "#A0A2EB",
